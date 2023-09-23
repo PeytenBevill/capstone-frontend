fb012478386ff4261eadcad691d6125ede35df63
PeytenBevill <redsage00@gmail.com>
2023-09-23 18:42:58 -0500

    now can save color waves and greets user

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -16,6 +16,11 @@ const Dashboard = ({ user_id }) => {
   const [clicked, setClicked] = useState(false);
   const [postBodyVisible, setPostBodyVisible] = useState(data.map(() => false));
   const [petNames, setPetNames] = useState([]);
+  const [firstName, setFirstName] = useState("");
+
+  const [titleColor, setTitleColor] = useState("");
+  const [bodyColor, setBodyColor] = useState("");
+  const [fontColor, setFontColor] = useState("");
 
   const handleClick = () => {
     setClicked(!clicked);
@@ -30,7 +35,6 @@ const Dashboard = ({ user_id }) => {
         post_body: post_body,
       })
       .then((res) => {
-        console.log(res.data);
         setPostTitle("");
         setPostBody("");
         fetch(`https://capstone-backend-topaz.vercel.app/posts/${user_id}`)
@@ -56,21 +60,69 @@ const Dashboard = ({ user_id }) => {
   }, [user_id]);
 
   useEffect(() => {
+    fetch(`https://capstone-backend-topaz.vercel.app/posts/colors/${user_id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setTitleColor(data[0].post_title_color);
+        setBodyColor(data[0].post_body_color);
+        setFontColor(data[0].post_font_color);
+      });
+  }, []);
+
+  useEffect(() => {
     fetch(`https://capstone-backend-topaz.vercel.app/users/${user_id}`)
       .then((res) => res.json())
       .then((petNames) => setPetNames(petNames));
-  }, [petNames]);
+  }, []);
+
+  useEffect(() => {
+    fetch(`https://capstone-backend-topaz.vercel.app/users/user/${user_id}`)
+      .then((res) => res.json())
+      .then((firstName) => setFirstName(firstName[0].first_name));
+  }, []);
+
+  const handlePostColor = (colorInfo) => {
+    const { titleColor, bodyColor, fontColor } = colorInfo;
+
+    setTitleColor(titleColor);
+    setBodyColor(bodyColor);
+    setFontColor(fontColor);
+
+    const postData = {
+      post_title_color: titleColor,
+      post_body_color: bodyColor,
+      post_font_color: fontColor,
+    };
+
+    axios
+      .put(
+        `https://capstone-backend-topaz.vercel.app/posts/${user_id}`,
+        postData
+      )
+      .then((response) => {
+        console.log("Colors updated successfully", response.data);
+      })
+      .catch((error) => {
+        console.error("Error updating colors:", error);
+      });
+  };
 
   return (
     <>
       <div className="screenBody">
-        <Toolkit className='toolkit'/>
+        <Toolkit
+          className="toolkit"
+          handlePostColor={handlePostColor}
+          firstName={firstName}
+        />
         {petNames.map((petName, index) => (
           <div key={index} className="top-of-page">
             <h2>
               Instructions for {petName.pet1}
-              {petName.pet2 && !petName.pet3? ` and ${petName.pet2}` : ""}
-              {petName.pet3 && petName.pet2 ? `, ${petName.pet2}, and ${petName.pet3}` : ""}
+              {petName.pet2 && !petName.pet3 ? ` and ${petName.pet2}` : ""}
+              {petName.pet3 && petName.pet2
+                ? `, ${petName.pet2}, and ${petName.pet3}`
+                : ""}
             </h2>
           </div>
         ))}
@@ -79,14 +131,21 @@ const Dashboard = ({ user_id }) => {
         ) : (
           data.map((x, index) => (
             <>
-              <div key={x.id} className="post-top">
+              <div
+                key={x.id}
+                className="post-top"
+                style={{ backgroundColor: titleColor, color: fontColor }}
+              >
                 <h3 className="post_title">{x.post_title}</h3>
                 <Switch
                   className="toggle-switch"
                   onClick={() => togglePostBody(index)}
                 />
               </div>
-              <div className="post-bottom">
+              <div
+                className="post-bottom"
+                style={{ backgroundColor: bodyColor, color: fontColor }}
+              >
                 {postBodyVisible[index] && (
                   <p className="post_body">{x.post_body}</p>
                 )}
--- src/components/Toolkit/Toolkit.jsx
+++ src/components/Toolkit/Toolkit.jsx
@@ -2,12 +2,48 @@ import { useState, useEffect } from "react";
 import { Button } from "@mui/material";
 import "./toolkit.css";
 
-const Toolkit = () => {
+const Toolkit = (props) => {
+  const colorWaves = [
+    { postTitle: "#bfd0e0", postBody: "#b8b3be", color: "#000000" },
+    { postTitle: "#093824", postBody: "#208394", color: "#ffffff" },
+    { postTitle: "#819595", postBody: "#B1B6A6", color: "#000000" },
+    { postTitle: "#6F2DBD", postBody: "#B298DC", color: "#ffffff" },
+    { postTitle: "#F7B05B", postBody: "#0C1713", color: "#ffffff" },
+    { postTitle: "#739E82", postBody: "#F3FFB6", color: "#000000" },
+    { postTitle: "#5A0002", postBody: "#33673B", color: "#ffffff" },
+    { postTitle: "#ffffff", postBody: "#d6d6d6", color: "#000000" },
+  ];
   const [clicked, setClicked] = useState(false);
+  const [titleColor, setTitleColor] = useState(colorWaves[7].postTitle);
+  const [bodyColor, setBodyColor] = useState(colorWaves[7].postBody);
+  const [fontColor, setFontColor] = useState(colorWaves[7].color)
 
   const handleClick = () => {
     setClicked(!clicked);
   };
+
+  const handleColor = (e) => {
+    const indexNum = e.target.getAttribute("data-color-identifier");
+    const newTitleColor = colorWaves[indexNum].postTitle;
+    const newBodyColor = colorWaves[indexNum].postBody;
+    const newFontColor = colorWaves[indexNum].color;
+  
+    setTitleColor(newTitleColor);
+    setBodyColor(newBodyColor);
+    setFontColor(newFontColor);
+  
+    props.handlePostColor({ titleColor: newTitleColor, bodyColor: newBodyColor, fontColor: newFontColor });
+  };
+  // const handleColor = (e) => {
+  //   const indexNum = e.target.getAttribute("data-color-identifier")
+
+  //   setTitleColor(colorWaves[indexNum].postTitle)
+  //   setBodyColor(colorWaves[indexNum].postBody)
+  //   setFontColor(colorWaves[indexNum].color)
+  //   // console.log(titleColor, bodyColor, fontColor)
+  //   props.handlePostColor({titleColor, bodyColor, fontColor})
+  // };
+  // console.log(props.firstName)
   return (
     <>
       <Button
@@ -19,13 +55,16 @@ const Toolkit = () => {
       </Button>
       {clicked && (
         <div className="animate__animated animate__bounceInDown tools">
+          <p>Greetings {props.firstName}</p>
           <div className="colors">
-            <button className="button1"></button>
-            <button className="button2"></button>
-            <button className="button3"></button>
-            <button className="button4"></button>
-            <button className="button5"></button>
-            <button className="button6"></button>
+            {colorWaves.map((colors, index) => (
+              <button
+                key={index}
+                className={`button${index + 1}`}
+                onClick={handleColor}
+                data-color-identifier={index}
+              ></button>
+            ))}
           </div>
         </div>
       )}
